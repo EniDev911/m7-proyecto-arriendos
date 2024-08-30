4cce410761addfcd1013c7fc25f500206cedf8cd
enidev911 <enidev911@gmail.com>
2024-08-30 15:18:31 -0400

    change: add crispy_forms

--- _site/settings.py
+++ _site/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'arrienda_ya'
+    'arrienda_ya',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
