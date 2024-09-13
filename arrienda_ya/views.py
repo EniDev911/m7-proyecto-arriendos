5d7e589483860eb2ae9dc6c3c9159eeb84533dc2
enidev911 <enidev911@gmail.com>
2024-09-13 15:59:51 -0300

    change: add register_tipo_view

--- arrienda_ya/views.py
+++ arrienda_ya/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import UserForm
+
+from .models import Profile, TipoUsuario, Usuario
+from .forms import TipoForm, UserForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 def index_view(request):
@@ -15,3 +18,26 @@ def register_view(request):
   else:
     form = UserForm()
   return render(request, 'registration/register.html', {'form': form})
+
+def register_tipo_view(request):
+  username = request.GET['user']
+  if request.method == "POST":
+    form = TipoForm(request.POST)
+    if form.is_valid():
+      tipo = form.cleaned_data['tipo']
+      rut = form.cleaned_data['rut']
+      direccion = form.cleaned_data['direccion']
+      telefono = form.cleaned_data['telefono']
+      user = User.objects.filter(username=username)[0]
+      tipo_user = TipoUsuario.objects.filter(id=int(tipo))[0]
+      profile_user = Profile(user=user,
+                             id_tipo_user=tipo_user,
+                             rut=rut,
+                             direccion=direccion,
+                             telefono=telefono,
+                             )
+      profile_user.save()
+      return HttpResponseRedirect('/login/')
+  else:
+    form = TipoForm()
+  return render(request, 'registration/register_tipo.html', {'form': form})
