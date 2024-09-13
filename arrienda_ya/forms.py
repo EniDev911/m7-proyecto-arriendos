e8dc7b36a8a6dadd1670a6b6692c9eb3e9255246
enidev911 <enidev911@gmail.com>
2024-09-13 05:29:37 -0300

    add: forms.py

--- arrienda_ya/forms.py
+++ arrienda_ya/forms.py
@@ -1,23 +1,22 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Comuna, Region
 
 class UserForm(UserCreationForm):
-  first_name = forms.CharField()
-  last_name = forms.CharField()
-  email = forms.EmailField()
+  email = forms.EmailField(label="Correo electrónico")
+
+  password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+  password2 = forms.CharField(label='Confirme Contraseña', widget=forms.PasswordInput)
+
   class Meta:
     model = User
-    fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
-
+    fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+    labels = {'username': ('Nombre de Usuario')}
 
 class TipoForm(forms.Form):
-  tipos = ((1, 'arrendatario'), (2, 'arrendador'), )
+  tipos = ((1, 'Arrendatario'), (2, 'Arrendador'))
   tipo = forms.ChoiceField(choices=tipos)
-  rut = forms.CharField(label='rut', max_length=100)
-  direccion = forms.CharField(label='direccion', max_length=100)
-  telefono = forms.CharField(label='telefono', max_length=100)
-
-class UserUpdateForm(forms.ModelForm):
-  class Meta:
-    fields = ['first_name', 'last_name', 'email']
+  rut = forms.CharField(label="rut", max_length=100)
+  direccion = forms.CharField(label="direccion", max_length=100)
+  telefono = forms.CharField(label="teléfono", max_length=100)
