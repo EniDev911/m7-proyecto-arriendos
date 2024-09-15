17d47387a65566a5866d5d83e8623a2d4567fc3f
enidev911 <enidev911@gmail.com>
2024-09-15 00:55:06 -0300

    change: update path in login

--- arrienda_ya/urls.py
+++ arrienda_ya/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
   path('', views.index_view, name='home_url'),
-  path('login/', LoginView.as_view(next_page='dashboard'), name='login_url'),
+  path('accounts/login/', LoginView.as_view(next_page='dashboard'), name='login_url'),
   path('logout/', LogoutView.as_view(next_page='home_url'), name='logout_url'),
   path('register/', views.register_view, name='register_url'),
   path('register_tipo/', views.register_tipo_view, name='register_tipo_url'),
